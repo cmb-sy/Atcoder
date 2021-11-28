670ba577e4bc5b42c298d091725f7b99e4276e04
nakashima1125 <cmbsy.quant@gmail.com>
2021-11-28 16:20:52 +0900

    remove no need code

--- Ruby/C/C-ChristmasEve.rb
+++ Ruby/C/C-ChristmasEve.rb
@@ -1,23 +1,18 @@
-# https://atcoder.jp/contests/abc115/tasks/abc115_c
 n, k = gets.chomp.split(" ").map(&:to_i)
 ary = []
 while input = gets
-  ary << (input.chomp.split(' ').map(&:to_i)).sort.flatten
+  ary << (input.chomp.split(' ').map(&:to_i))
 end
-
+ary = ary.sort.flatten
 ans = []
-max = ary.max
-min = ary.min
+ 
+j = 0
+i = k - 1
 
-if ary.count{|x|x==min} == k || ary.count{|x|x==max} == k 
-  puts 0
-else
-  j = 0
-  i = k - 1
-  (n-k+1).times do  
-    ans.push(ary[i] - ary[j])
-    j += 1
-    i += 1
-  end
-  puts ans.min
+(n-k+1).times do  
+  ans.push(ary[i] - ary[j])
+  j += 1
+  i += 1
 end
+
+puts ans.min
