7df39b78353e77b51a4651d9a341b279e96b80f1
nakashima <nakashima@nakashimanoMacBook-Air.local>
2022-05-12 07:09:47 +0900

    tmo condition

---
+++ Java/C/Otoshidama.java
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/abc085/tasks/abc085_c
+import java.util.*;
+
+class Main {
+          public static void main(String[] args) {
+                    Scanner scan = new Scanner(System.in);
+      
+                    int n = scan.nextInt();
+                    int y = scan.nextInt();
+
+                    int[] number = { -1, 1, -1 };
+                    for (i=0; i<n;)
+          }
+}
